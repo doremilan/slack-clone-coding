55f3c623baf412f15aea065d7765c9f5f56cbac0
Milan Lee <100390926+doremilan@users.noreply.github.com>
2022-09-16 19:03:43 +0900

    fix: config 모듈 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('abc')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('user') // GET /abc/user
+  getUser(): string {
+    return this.appService.getUser();
+  }
+
+  @Post('user') // Post /abc/user
+  postUser(): string {
+    return this.appService.postUser();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getUser(): string {
+    return 'Hello World!';
+  }
+
+  postUser(): string {
     return 'Hello World!';
   }
 }
